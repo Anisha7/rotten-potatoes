559a204b6f907b5435335a4bd9fdbb27ee1aaaf7
Anisha Jain <anishajain@outlook.com>
2018-09-04 10:34:06 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true 
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  movieRating: String,
 });
 
 // INDEX
@@ -46,15 +47,20 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
     console.log(err.message);
   })
 })
 
-
-
-
+// SHOW (url/request parameter)
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
