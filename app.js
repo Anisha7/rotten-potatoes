07d4c9568344c4d380f35ee05e799242dbe5b4cb
Anisha Jain <anishajain@outlook.com>
2018-09-04 10:04:04 -0700

    Added mongo database

--- app.js
+++ app.js
@@ -8,16 +8,33 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 // initialized
 
+// initializing mongoose and connecting to data base
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+// add a model to review
+const Review = mongoose.model('Review', {
+  title: String
+});
+
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" },
-  { title: "Previous Review" }
-]
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Next Review" },
+//   { title: "Previous Review" }
+// ]
 
 // INDEX
+// The find() method returns a Promise. A Promise is an object that represents a value that will be provided in the future.
+// Call .then() and provide a function for the Promise to call when it resolves
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.listen(3000, () => {
